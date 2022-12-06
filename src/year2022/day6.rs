2b77ef2094a0cbe02a2dfea4eb0fa330f1408865
Emma Böcker <stckoverflw@gmail.com>
2022-12-06 08:52:11 +0000

    use functions in day 6 because @NyCodeGHG is annoying as shit

--- src/year2022/day6.rs
+++ src/year2022/day6.rs
@@ -1,3 +1,5 @@
+use std::str::Chars;
+
 use super::AdventOfCode2022;
 use advent_of_code_traits::{days::Day6, ParseInput, Part1, Part2, Solution};
 
@@ -15,23 +17,7 @@ impl Solution<'_, Day6, Part1> for AdventOfCode2022<Day6> {
     type Output = i32;
 
     fn solve(&self, input: &String) -> Self::Output {
-        let mut count = 4;
-
-        for chars in input.chars().collect::<Vec<_>>().windows(4) {
-            let abc = chars.into_iter().enumerate().any(|(i, x)| {
-                let mut chars = chars.to_vec();
-                chars.remove(i);
-                chars.into_iter().any(|y| x == &y)
-            });
-
-            if abc {
-                count += 1;
-            } else {
-                break;
-            }
-        }
-
-        count
+        check_first_unique_sequene(input.chars(), 4)
     }
 }
 
@@ -41,22 +27,26 @@ impl Solution<'_, Day6, Part2> for AdventOfCode2022<Day6> {
     type Output = i32;
 
     fn solve(&self, input: &String) -> Self::Output {
-        let mut count = 14;
-
-        for chars in input.chars().collect::<Vec<_>>().windows(14) {
-            let abc = chars.into_iter().enumerate().any(|(i, x)| {
-                let mut chars = chars.to_vec();
-                chars.remove(i);
-                chars.into_iter().any(|y| x == &y)
-            });
-
-            if abc {
-                count += 1;
-            } else {
-                break;
-            }
-        }
-
-        count
+        check_first_unique_sequene(input.chars(), 14)
     }
+}
+
+fn check_first_unique_sequene(chars_input: Chars, window_size: usize) -> i32 {
+    let mut count = window_size.clone() as i32;
+
+    for chars in chars_input.collect::<Vec<_>>().windows(window_size) {
+        let abc = chars.into_iter().enumerate().any(|(i, x)| {
+            let mut chars = chars.to_vec();
+            chars.remove(i);
+            chars.into_iter().any(|y| x == &y)
+        });
+
+        if abc {
+            count += 1;
+        } else {
+            break;
+        }
+    };
+
+    count
 }
