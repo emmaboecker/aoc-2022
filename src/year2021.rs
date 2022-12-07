cf1c0f3c0404ece867b3d56ef4adadff8dcd2615
Emma Böcker <stckoverflw@gmail.com>
2022-12-07 16:25:42 +0100

    dont use the weird trait things anymore

--- src/year2021.rs
+++
@@ -1,5 +0,0 @@
-mod day1;
-mod day2;
-mod day3;
-
-pub struct AdventOfCode2021<const DAY: u32>;
--- src/year2022.rs
+++
@@ -1,9 +0,0 @@
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-
-pub struct AdventOfCode2022<const DAY: u32>;
